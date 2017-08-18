a317ed76d7632e40b9e1c8d718c441f012fb6667
Георгий Бухаров <gbu@crtweb.ru>
2017-08-18 18:14:54 +0500

    WiP

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -8,33 +8,81 @@ $(window).on('mouseup', function() {
 })
 
 $(function() {
-  var display = document.querySelector('.calculator__screen');
+  var display = document.querySelector('.calculator__screen'),
+      subdisplay = document.querySelector('.calculator__supscreen'),
+      res = '',
+      cutres = '';
+
+      $('[data-oper]').on('click', function() {
+        $('[data-oper]').removeAttr('disabled', '');
+        $(this).attr('disabled', '');
+      })
+
       $('.calc-btn').on('click', function() {
 
+        if ($(this).data('oper')) {
+          res += display.textContent + $(this).data('oper');
+        }
+
+        // if ($(this).data('oper')&&/\d(\*|\/|\-|\+|\.)/g.test(res)) {
+        //   res = display.textContent + $(this).data('oper')
+        // }
+
+        // if (/(\-?(?:\d*\.)?\d+(\*|\/|\-|\+)(?:\d*\.)?\d+)/g.test(res)) {
+
+        // }
+
+        // if(/(\*|\/|\-|\+|\.){2,}/g.test(res)) {
+
+        // }
+
+        // Clear screen after result
+        if ((display.textContent == cutres) || subdisplay.textContent[subdisplay.textContent.length-1]=='+' || subdisplay.textContent[subdisplay.textContent.length-1]=='-' || subdisplay.textContent[subdisplay.textContent.length-1]=='*' || subdisplay.textContent[subdisplay.textContent.length-1]=='/') {
+          display.textContent = ''
+        }
+
         display.textContent += $(this).val();
+
+        subdisplay.textContent += $(this).val() || $(this).data('oper');
+
+        cutres = res.slice(0, res.length - 1);
+
+        // subdisplay.textContent = exp
+
+        console.log('res: ', res);
+        console.log('exp: ', subdisplay.textContent);
+        console.log('cutres: ', cutres);
+        console.log('display: ', display.textContent);
+
+
+        if (/\d{17}/g.test(subdisplay.textContent)||/((?:\d\.)?\d){16,}/g.test(subdisplay.textContent)||/\d+\.\d+\./g.test(subdisplay.textContent)) {
+          subdisplay.textContent = subdisplay.textContent.slice(0, subdisplay.textContent.length - 1)
+        }
+
         // Check if there is more than 1 operator or dot at row and cut it
-        if(/(\*|\/|\-|\+|\.){2,}/g.test(display.textContent)) {
-          display.textContent = display.textContent.slice(0, display.textContent.length-2) + $(this).val();
+        if(/(\*|\/|\-|\+|\.){2,}/g.test(subdisplay.textContent)) {
+          subdisplay.textContent = subdisplay.textContent.slice(0, subdisplay.textContent.length-2) + $(this).data('oper');
+          res = res.slice(0, res.length-2) + $(this).data('oper');
         }
         //Check if operator or dot is first char and cut it, except single '-'
-        if (display.textContent.indexOf('+')==0||display.textContent.indexOf('*')==0||display.textContent.indexOf('/')==0 || display.textContent.indexOf('.')==0 || /\-{2,}/g.test(display.textContent)) {
-          display.textContent = display.textContent.slice(1, display.textContent.length);
+        if (subdisplay.textContent.indexOf('+')==0 || subdisplay.textContent.indexOf('*')==0 || subdisplay.textContent.indexOf('/')==0 || subdisplay.textContent.indexOf('.')==0 || /\-{2,}/g.test(subdisplay.textContent)) {
+          subdisplay.textContent = subdisplay.textContent.slice(1, subdisplay.textContent.length);
         }
 
         if (display.textContent.length >= 17 || /\d+\.\d+\./g.test(display.textContent)) {
           display.textContent = display.textContent.slice(0, display.textContent.length-1)
         }
 
-        else if (checkIndex(display.textContent)>0) {
+        else if (checkIndex(cutres)>0) {
 
-          var middleIndex = checkIndex(display.textContent),  //Find index of operator in expression
-              operand = display.textContent[middleIndex],     //Find an operator
-              rightSearch = middleIndex + 1,                  //Define right part of expression
+          var middleIndex = checkIndex(cutres),  //Find index of operator in expression
+              operand = cutres[middleIndex],     //Find an operator
+              rightSearch = middleIndex + 1,     //Define right part of expression
               right = '';
 
             //Parse right part of expression
-            while (rightSearch < display.textContent.length) {
-              right = right + display.textContent[rightSearch];
+            while (rightSearch < cutres.length) {
+              right = right + cutres[rightSearch];
               rightSearch++;
             }
 
@@ -44,7 +92,7 @@ $(function() {
 
             //Parse left part of expression
             while (leftSearch >= 0) {
-              left = display.textContent[leftSearch] + left;
+              left = cutres[leftSearch] + left;
               leftSearch--;
             }
 
@@ -54,23 +102,25 @@ $(function() {
             case '+':
               if(+left>0&&+left<1&&+right>0&&+right<1) {
 
-                display.textContent = dotNumbers(+left, +right);
+                cutres = dotNumbers(+left, +right);
 
               }
-              else display.textContent = +left + +right;
+              else cutres = +left + +right;
               break;
 
             case '-':
-              display.textContent = +left - +right;
+              cutres = +left - +right;
               break;
             case '*':
-              display.textContent = +left * +right;
+              cutres = +left * +right;
               break;
 
             case '/':
-              display.textContent = +left / +right;
+              cutres = +left / +right;
               break;
           }
+          display.textContent = cutres;
+          res = String(cutres) + $(this).data('oper');
         }
       });
 }($));
